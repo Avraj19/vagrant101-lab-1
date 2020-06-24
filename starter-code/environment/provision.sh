23199e998545a0bfaa65d0753da4ef17485cf22a
Avraj19 <avrajsbw@gmail.com>
2020-06-24 10:06:02 +0100

    update provision.sh

--- starter-code/environment/provision.sh
+++ starter-code/environment/provision.sh
@@ -6,7 +6,11 @@ sudo apt-get install nginx -y
 service nginx start
 
 
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
 sudo apt-get install -y nodejs
-npm install -g pm2
+
+sudo npm install pm2 -g
+
+# cd /app
+# npm install
+# npm start
